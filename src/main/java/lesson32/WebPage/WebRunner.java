2dd9e17e24857f318087ef14096cbba5d2e0856e
igor <anokhin.igor@mail.ru>
2022-03-24 11:08:04 +0300

    lesson 32. Web finished

--- src/main/java/lesson32/WebPage/WebRunner.java
+++ src/main/java/lesson32/WebPage/WebRunner.java
@@ -2,11 +2,15 @@ package lesson32.WebPage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+Сверстать простенькую HTML-страницу, на которой разместить текстовые поля, чекбоксы, комбобоксы
+и прочие HTML-элементы, кнопку для отправки данных, обернув их в форму.
+Также предлагается добавить CSS-правил и код JS для валидации передаваемых клиентом данных.
+ */
 
 @SpringBootApplication
 public class WebRunner {
     public static void main(String[] args) {
-        SpringApplication.run(WebRunner.class,args);
+        SpringApplication.run(WebRunner.class, args);
     }
-
 }
